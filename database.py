9ea3594f52c5430ff2befa8350c26e25f15d360d
Ganfen <473243033@qq.com>
2026-09-16 13:03:20 +0800

    feat： 连接数据库模块

---
+++ database.py
@@ -0,0 +1,37 @@
+import re
+from typing import Tuple
+from asyncio import wait
+import pymysql
+from config import database_config
+
+class DatabaseClient:
+    def __init__(self):
+        self.config=database_config
+
+
+    def execute(self,sql : str) ->Tuple[list[str],list[tuple]]:
+        """执行sql"""
+        conn=pymysql.connect(**self.config)
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(sql)
+                if cursor.description:
+                    columns = [desc[0] for desc in cursor.description]
+                else:
+                    columns = []
+                results = cursor.fetchall()
+            return  columns,results
+        finally:
+            conn.close()
+
+    def validate(self):
+        """验证数据库连接是否正常"""
+        try:
+            conn=pymysql.connect(**self.config)
+            conn.close()
+            return True
+        except Exception:
+            return False
+
+if __name__=='__main__':
+    pass
